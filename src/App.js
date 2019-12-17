c82a9e749c3db94e1aa1a360ad6042b39cf17205
Safia MSELLEK-ISMAILI <sms@cnam-lyon.peda>
2019-12-17 14:49:05 +0100

    first interface created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.scss';
 import './index.scss'
 import Nav from "./Nav";
 
-function App() {
-  return (
-    <>
-    <Nav />
-    <div className="App">
-      <header className="App-header">
-          Welcome to MeetUp
-    
-      </header>
-    </div>
-    </>
-  );
+class App extends Component {
+  render = () => {
+    return (
+      <>
+        <Nav />
+        <div className="container">
+          <Router>
+            <div>
+              <Switch>
+                {/* <Route path="/user/:id" component={UserPage} />
+                <Route exact path="/" component={Users} />
+                <Route component={NotFound} /> */}
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </>
+    );
+  };
 }
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class Nav extends Component {
   render = () => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">Users</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">Home</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <a className="navbar-brand" href="/">List</a>
+      <a className="navbar-brand" href="/">Login</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
